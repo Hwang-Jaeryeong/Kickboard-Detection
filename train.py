5e6896d6c9fdfccde3d8f02a0ac321cb499c1f3d
Keon Lee <22269190+klee9@users.noreply.github.com>
2024-10-03 16:35:30 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,31 +1,24 @@
 from ultralytics import YOLO
-import argparse
 
-# Create argument parser
-parser = argparse.ArgumentParser(description="Model Training Script")
-
-parser.add_argument('--data', type=str, required=True, help='Path to the dataset YAML file')
-parser.add_argument('--name', type=str, required=True, help='Name of the experiment')
-parser.add_argument('--project', type=str, required=True, help='Project name or directory to save results')
-parser.add_argument('--epochs', type=int, required=True, help='Number of epochs to train')
-parser.add_argument('--imgsz', type=int, default=640, help='Image size (default = 640)')
-parser.add_argument('--batch', type=int, default=16, help='Batch size (default = 16)')
-parser.add_argument('--lr', type=float, default=0.01, help='Initial learning rate (default = 0.01)')
-
-# Parse arguments
-args = parser.parse_args()
+DATA = input("Path to the dataset YAML file: ")
+NAME = input("Name of the experiment: ")
+PROJECT = input("Project name or directory to save results: ")
+EPOCHS = input("Number of epochs to train: ")
+IMGSZ = input("Image size: ")
+BATCH = input("Batch size: ")
+LR = input("Initial learning rate: ")
 
 # Change this to desired version
 model = YOLO('yolov8s.pt')
 
 # Train with given arguments
 model.train(
-    data=args.data,
-    epochs=args.epochs,
-    imgsz=args.imgsz,
-    batch=args.batch,
-    name=args.name,
-    project=args.project,
+    data=DATA,
+    epochs=EPOCHS,
+    imgsz=IMGSZ,
+    batch=BATCH,
+    name=NAME,
+    project=PROJECT,
     save_period=1,
-    lr0=args.lr
+    lr0=LR
 )
