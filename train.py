ffd713334fcb0668cb5997e16dac7a4445278a9a
Keon Lee <22269190+klee9@users.noreply.github.com>
2024-10-03 16:46:26 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,10 +3,11 @@ from ultralytics import YOLO
 DATA = input("Path to the dataset YAML file: ")
 NAME = input("Name of the experiment: ")
 PROJECT = input("Project name or directory to save results: ")
-EPOCHS = input("Number of epochs to train: ")
-IMGSZ = input("Image size: ")
-BATCH = input("Batch size: ")
-LR = input("Initial learning rate: ")
+EPOCHS = int(input("Number of epochs to train: "))
+IMGSZ = int(input("Image size: "))
+BATCH = int(input("Batch size: "))
+LR = float(input("Initial learning rate: "))
+AUG = input("Augment? (y/n): ")
 
 # Change this to desired version
 model = YOLO('yolov8s.pt')
@@ -20,5 +21,6 @@ model.train(
     name=NAME,
     project=PROJECT,
     save_period=1,
-    lr0=LR
+    lr0=LR,
+    augment=AUG
 )
